978562f1b1740339eb4e5c5cb72aa8c230f374a3
Manuela Munoz <manuelamunoz103@gmail.com>
2021-01-04 07:00:10 -0500

    Legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -117,5 +117,20 @@ function createMap(earthquakes) {
     L.control.layers(baseMaps, overlayMaps, {
         collapsed:false
     }).addTo(myMap);
+    
+    // Create a legend control
+    var legend = L.control({position:'bottomright'});
+    legend.onAdd = function (map) {
+      var div = L.DomUtil.create('div', 'info legend'),
+      grades = [-10, 10, 30, 50, 70, 90];
 
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i=0; i < grades.length; i++){
+      div.innerHTML +=
+         '<i style="background:' + chooseColor(grades[i] + 1) + '"></i> ' +
+         grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+    };
+    legend.addTo(myMap);
 }
